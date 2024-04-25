1a6bed4fca5e53da8166c1643b28fdc9966ba67b
yozakuragekkouka <tsukitohikaritoyami@icloud.com>
2024-04-25 13:06:38 +0900

    bg

--- Src/Nishiyama/BackGround/BackGround.cpp
+++ Src/Nishiyama/BackGround/BackGround.cpp
@@ -15,8 +15,28 @@ BackGround::~BackGround()
 
 void BackGround::Init()
 {
+	ResultBackCount = 0;
+
 	for (int i = 0; i < BACK_GROUND_NUM; i++)
 	{
 		//BG_Image[i].RectInit();
 	}
+}
+
+void BackGround::Step()
+{
+	/*for (int i = 0; i < BACK_GROUND_NUM; i++)
+	{
+		BG_Image[i].Move_Y((float)-BACK_GROUND_SPEED);
+		if (ResultBack[i].Get_pos().y <= -(RESULT_BACK_SIZEH + ((RESULT_BACK_OVER * ResultBackCount) % RESULT_BACK_SPEED)))
+		{
+			ResultBack[i].Set_posY((float)(RESULT_BACK_SIZEH * (RESULT_BACK_NUM - 1) + ((RESULT_BACK_OVER * ResultBackCount) % RESULT_BACK_SPEED)));
+
+			ResultBackCount++;
+			if (RESULT_BACK_OVER == (RESULT_BACK_OVER * ResultBackCount) % RESULT_BACK_SPEED)
+			{
+				ResultBackCount = 0;
+			}
+		}
+	}*/
 }
--- Src/Nishiyama/BackGround/BackGround.h
+++ Src/Nishiyama/BackGround/BackGround.h
@@ -4,9 +4,14 @@
 constexpr int BACK_GROUND_NUM = 3;
 constexpr int BACK_GROUND_SPEED = 3;
 
+constexpr int BACK_GROUND_SIZEW = 1280;
+constexpr int BACK_GROUND_SIZEH = 720;
+constexpr int BACK_GROUND_OVER = BACK_GROUND_SPEED - (BACK_GROUND_SIZEW % BACK_GROUND_SPEED);
+
 class BackGround
 {
 private:
+	int ResultBackCount;
 	Rect_Data BG_Image[BACK_GROUND_NUM];
 
 public:
