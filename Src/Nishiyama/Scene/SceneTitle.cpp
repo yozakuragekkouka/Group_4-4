d3b6969e3993b5fcd0e510eaa1e724c04f3371e5
Maruru3n <fukushimaks121@icloud.com>
2024-04-30 10:51:15 +0900

    タイトル急ピッチ仮

--- Src/Nishiyama/Scene/SceneTitle.cpp
+++ Src/Nishiyama/Scene/SceneTitle.cpp
@@ -6,21 +6,43 @@
 //タイトル初期化
 void SceneTitle::Init()
 {
+	handle[0] = LoadGraph("Data/Image/titleName.png");
+	handle[1] = LoadGraph("Data/Image/titleUI.png");
+	alpha=255;
+	alphaFlag=false;
 	SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_LOOP_TITLE;
 }
 
 //タイトル通常処理
 void SceneTitle::Step()
 {
-	if (Input::PressAnyKey())
-	{
+
+	if (Input::IsKeyPush(KEY_INPUT_RETURN)) {
 		SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_FIN_TITLE;
+	}
+	if (alphaFlag) {
+		alpha += 5;
+		if (alpha >= 255) {
+			alpha = 255;
+			alphaFlag = false;
+		}
+	}
+	if (!alphaFlag) {
+		alpha -= 5;
+		if (alpha <= 0) {
+			alpha = 0;
+			alphaFlag = true;
+		}
 	}
 }
 
 //タイトル描画処理
 void SceneTitle::Draw()
 {
+	DrawGraph(-10, 0, handle[0], true);
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, alpha);
+	DrawGraph(0, 50, handle[1], true);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND,0);
 }
 
 //タイトル後処理
--- Src/Nishiyama/Scene/SceneTitle.h
+++ Src/Nishiyama/Scene/SceneTitle.h
@@ -5,7 +5,9 @@
 class SceneTitle : public SceneBase
 {
 private:
-
+	int handle[2];
+	int alpha;
+	bool alphaFlag;
 public:
 	void Init();
 
